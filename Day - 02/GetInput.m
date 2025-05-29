9f31917c0764a733ce5a1b8cdd2877589fb50171
Lakruwani Pabasara <lakruwanipabasara14@gmail.com>
2025-05-29 15:35:29 +0530

    Add files via upload

---
+++ Day - 02/GetInput.m
@@ -0,0 +1,5 @@
+x=input('Enter the number: ');
+disp(['You entered ',num2str(x)]);
+
+name=input('Enter your name: ','s');
+disp(['Hello ',name]);
---
+++ Day - 02/forloop.m
@@ -0,0 +1,9 @@
+for variable=1:5
+    disp(variable)
+end
+
+for variable=1:2:10
+    disp(variable)
+end
+
+
