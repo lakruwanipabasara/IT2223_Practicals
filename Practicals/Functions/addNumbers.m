06bf023f54c4d5c4c5a98a6dfaf86cbdbde2ddc7
Lakruwani Pabasara <lakruwanipabasara14@gmail.com>
2025-06-04 15:40:59 +0530

    Add files via upload

---
+++ Practicals/Functions/addNumbers.m
@@ -0,0 +1,5 @@
+function sum = addNumbers(x, y)
+    sum = x + y;
+end
+
+% this function input and output
---
+++ Practicals/Functions/myFunction.m
@@ -0,0 +1,3 @@
+function myFunction()
+ disp('Hello, this function has no inputs and outputs');
+end
